254c51a148905db5d6d25879def4e68e2704df37
Алексей <alexburnitdown@gmail.com>
2020-09-22 16:30:21 +0300

    desktop ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -79,9 +79,24 @@ module.exports = {
       '14': '4.5rem',
       '15': '5rem',
     },
+    triangles: { // defaults to {}
+      'left': {
+        direction: 'left',      // one of 'left', 'right', 'up', 'down', 'left-up', 'left-down', 'right-up', and 'right-down'
+        size: '1em',            // defaults to defaultSize
+        height: '0.5em',        // defaults to half the size; has no effect on the diagonal directions (e.g. 'left-up')
+        color: 'currentColor',  // defaults to defaultColor
+      },
+    },
   },
   variants: {
-    backgroundColor: ['responsive', 'hover', 'focus', 'active']
+    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+    triangles: ['responsive'] // defaults to []
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-triangles')({
+      componentPrefix: 'c-',        // defaults to 'c-'
+      defaultSize: '1em',           // defaults to '1em'
+      defaultColor: 'currentColor', // defaults to 'currentColor'
+    }),
+  ],
 }
